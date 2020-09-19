301fd6527e541d1729ddfaaf308dd13f2ca4a2f8
Max Hennum <mphennum@gmail.com>
2020-09-19 23:08:29 +0100

    light node class

---
+++ src/node/light.js
@@ -0,0 +1,24 @@
+import * as THREE from 'three';
+import Node from './node';
+
+const DEFAULTLIGHTDEPTH = 10;
+
+class Light extends Node {
+
+	constructor() {
+		super({ mesh: new THREE.DirectionalLight(0xFFFFFF, 1) });
+
+		this.translateTo({ z: DEFAULTLIGHTDEPTH * 2 });
+
+		this.mesh.castShadow = true;
+		this.mesh.shadow.camera.near = 0;
+		this.mesh.shadow.camera.far = DEFAULTLIGHTDEPTH * 2.5;
+		this.mesh.shadow.camera.left = -5.1;
+		this.mesh.shadow.camera.right = 5.1;
+		this.mesh.shadow.camera.top = 5.1;
+		this.mesh.shadow.camera.bottom = -5.1;
+	}
+
+}
+
+export default Light;
