fb19a71cc0b1801d8df235bf3194cea489e58519
Max Hennum <mphennum@gmail.com>
2020-09-20 20:19:33 +0100

    rollup output global to avoid warning about three

--- rollup.config.js
+++ rollup.config.js
@@ -6,40 +6,37 @@ const NAMESPACE = 'Threedchart';
 const FILENAME = 'threedchart';
 const PROD = (process.env.BUILD === 'production');
 
-let output = [
-	{
-		file: 'dist/threedchart.js',
-		format: 'iife',
-		name: NAMESPACE,
-	},
-];
-
-if (PROD) {
-	output = [
-		...output,
-		{
-			file: 'dist/index.js',
-			format: 'cjs',
-			exports: 'auto',
-		},
-		{
-			file: 'dist/threedchart.min.js',
-			format: 'iife',
-			name: NAMESPACE,
-			compact: true,
-			plugins: [ terser() ],
-		},
-	];
-}
-
 export default {
 	input: 'src/threedchart.js',
-	output,
+	output: [
+		{
+			file: 'dist/threedchart.js',
+			format: 'iife',
+			name: NAMESPACE,
+			globals: PROD ? null : {
+				'three': 'THREE',
+			},
+		},
+		...(PROD ? [
+			{
+				file: 'dist/index.js',
+				format: 'cjs',
+				exports: 'auto',
+			},
+			{
+				file: 'dist/threedchart.min.js',
+				format: 'iife',
+				name: NAMESPACE,
+				compact: true,
+				plugins: [ terser() ],
+			},
+		] : [ ]),
+	],
 	plugins: [
 		babel({ babelHelpers: 'bundled' }),
 		nodeResolve(),
 	],
-	external: PROD ? [ ] : [
+	external: PROD ? null : [
 		'three',
 		// /node_modules/,
 	],
